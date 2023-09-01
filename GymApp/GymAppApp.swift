856489e8865b87468cbbe6c0aa0746be803e262a
codyeeine <76597781+codyeeine@users.noreply.github.com>
2023-09-01 11:20:48 -0400

    Initial Commit

---
+++ GymApp/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  GymApp
+//
+//  Created by Cody Choo-Foo on 2023-09-01.
+//
+
+import SwiftUI
+
+@main
+struct GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
